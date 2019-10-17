91a9d8094268c135a021db9396df46b58d01f618
DanielAnthes <daniel.anthes97@gmail.com>
2019-10-17 20:00:44 +0200

    switchable loss

--- CartPole.py
+++ CartPole.py
@@ -1,4 +1,4 @@
-from Agents import ReinforceAgent, DQNAgent, Net, ReplayMemory
+from Agents_debug import ReinforceAgent, DQNAgent, Net, ReplayMemory
 from utils import save_agent, load_agent, annealing, random_beginning
 
 resume = False  # So
@@ -10,7 +10,7 @@ if resume:
 else:
     agent = DQNAgent(Net(4, 3, 2), Net(4, 3, 2), [0, 1], "CartPole-v1")
 
-agent.train(random_beginning, n_episodes=20000, ctg=False)
+agent.train(random_beginning, n_episodes=10000, ctg=False, update_interval=10)
 
 for _ in range(10):
     agent.run()
