67a2ebeb935d7ff43e617f055b19520dd3d76bbb
Moritz Nipshagen <mnipshagen@uos.de>
2019-12-12 17:23:10 +0100

    moved environment to src

--- src/environment.py
+++ src/environment.py
@@ -1,3 +1,30 @@
-@abstract
-class Environment:
-  pass
+import abc
+from typing import List
+
+
+class Environment(abc.ABC):
+
+    @abc.abstractmethod
+    def __init__(self, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def step(self, action) -> List[float], int, bool:
+        """
+        Returns environment as tuple: state, reward, done
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_actionspace(self) -> List:
+        """
+        Returns available actions as list
+        """
+        pass
+
+    @abc.abstractmethod
+    def reset(self) -> List[float]:
+        """
+        Returns reset state
+        """
+        pass
