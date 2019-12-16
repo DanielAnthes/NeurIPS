c344e8b7bac2a4538eade12a23366fb78b35b5b2
DanielAnthes <daniel.anthes97@gmail.com>
2019-12-16 17:29:57 +0100

    add test script for a3c in cartpole env

---
+++ src/RunCartpoleA3C.py
@@ -0,0 +1,5 @@
+from neurips2019.environments.CartpoleFactory import CartpoleFactory
+from neurips2019.agents.A3C import A3CAgent
+
+agent = A3CAgent(10, CartpoleFactory(), [0,1])
+agent.train(100) # train for total of 100 episodes
