eea43e56bd8c0e46e2e56fe9a693634717fcd935
mnipshagen <moritz.nipshagen@gmail.com>
2019-12-23 16:24:13 +0100

    Added documentation and more readability

--- src/controller.py
+++ src/controller.py
@@ -7,49 +7,68 @@ For curses to work on windows, third party binaries need to be installed.
 import sys
 import curses
 import time
+from enum import Enum
 
 from neurips2019.environments.neurosmash_environment import NeurosmashEnvironment as NSenv
 
-
+#############################
+### Environment Variables ###
+### Set them according to ###
+### Neurosmash settings   ###
+#############################
 ENV_TIMESCALE=5
 ENV_SIZE=768
 ENV_PORT=13000
 ENV_IP="127.0.0.1"
 
+class KeyCodes(Enum):
+    quit = -1
+    noact = 0
+    left = 1
+    right = 2
+    reset = 3
 
 def get_input(screen):
+    """
+    Grab the characters from screen.
+    """
     char = screen.getch()
     if char == ord('q'):
-        return -1
+        return KeyCodes.quit
     elif char == curses.KEY_RIGHT:
-        return 2
+        return KeyCodes.right
     elif char == curses.KEY_LEFT:
-        return 1
-    elif char == curses.KEY_DOWN:
-        return 0
+        return KeyCodes.left
     elif char == curses.KEY_BACKSPACE:
-        return 4
-    else:
-        return 0
+        return KeyCodes.reset
+    else: # handle all other keys as no action
+        return KeyCodes.noact
 
 def connect():
+    """Connect to Environment"""
     return NSenv(ENV_TIMESCALE, ENV_SIZE, ENV_PORT, ENV_IP)
 
 def run(env, screen):
+    """
+    Run the controller in a loop until Player quits.
+    """
     memory = []
     state = rew = done = nstate = None
     while True:
         _in = get_input(screen)
-        if _in == -1:
+        if _in == KeyCodes.quit:
             break
-        if _in == '': _in = 0
-        if _in == 4: 
+        # should technically not occur
+        if _in == '':
+            _in = KeyCodes.noact
+        if _in == KeyCodes.reset: 
             state = env.reset()
             rew = 0
             done = False
             continue
-        screen.addstr(1,0,str(_in))
-        nstate, rew, done = env.step(_in)
+            
+        screen.addstr(1,0,str(_in.value))
+        nstate, rew, done = env.step(_in.value)
         time.sleep(0.1 / ENV_TIMESCALE)
         memory.append((state, rew, done, nstate))
         state = nstate
@@ -57,6 +76,11 @@ def run(env, screen):
     return memory
 
 def save(result):
+    """
+    Save the episode in a format to enable imitation learning.
+    
+    To be implemented
+    """
     pass
 
 def main():
@@ -68,7 +92,7 @@ def main():
     curses.cbreak()
     # map arrow keys to special values
     screen.keypad(True)
-    #
+    # Add short info
     screen.addstr(0,0,"Left / Right Arrow to turn. Lower Arrow for straight. q to quit.\n")
     try:
         env = connect()
