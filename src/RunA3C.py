306b0e2e87df879f5660b6fbe65e31a029e1392d
Moritz Nipshagen <moritz.nipshagen@protonmail.com>
2020-01-02 17:01:20 +0100

    Abstracted and threaded logging

--- src/RunA3C.py
+++ src/RunA3C.py
@@ -1,97 +1,49 @@
 import pickle
 import os
 import matplotlib.pyplot as plt
+from torch.multiprocessing import Queue
+from threading import Thread
 from neurips2019.environments.LunarLanderFactory import LunarLanderFactory
 from neurips2019.environments.CartpoleFactory import CartpoleFactory
 from neurips2019.agents.A3C import A3CAgent
 from neurips2019.agents.Networks import Net
 from neurips2019.util.utils import annealing, slow_annealing
+from neurips2019.util.Logger import Logger
 
+from A3C_configs import get_config
 
 SAVE_DIR = "logs"
-########################
-### CONFIG FOR LUNAR ###
-########################
-def policy_net():
-    return Net(8,[32,32],4)
-def value_net():
-    return Net(8, [32,32],1)
-lunar_conf = {
-    "valuenet": value_net, # function returning a pytorch network to encode policy
-    "policynet": policy_net, # function returning a pytorch network to encode state values
-    "train_blocks": 1, # how often train is called
-    "block_size": 20000, # episodes per call to train
-    "num_workers": 4, # number of worker processes
-    "lookahead": 30, # steps to take before computing losses
-    "show_immediate": False, # show plots after each call to train
-    "keep_plots": True, # keep plots open after training has finished
-    "debug": False, # additional debug prints
-    "epsilon": slow_annealing, # exploration strategy
-    "policy_lr": 0.0001, # learning rate for policy net optimizer
-    "value_lr": 0.0001, # learning rate for valuenet optimizer
-    "policy_decay": 0.0001, # weight decay for policy optimizer
-    "value_decay": 0.0001, # weight decay for value optimizer
-    "env": LunarLanderFactory(), # environment factory object
-    "evaluate": 10, # number of episodes to play for evaluation
-    "grad_clip": 40, # max norm for gradients, used to clip gradients
-    "gamma": 0.99, # discount for future rewards
-    "actions": [0,1,2,3] # actions allowed in the environment
-}
-
-###########################
-### CONFIG FOR CARTPOLE ###
-###########################
-def policy_net_cp():
-    return Net(4,10,2)
-def value_net_cp():
-    return Net(4,10,1)
-cartpole_conf = {
-    "valuenet": value_net_cp, # function returning a pytorch network to encode policy
-    "policynet": policy_net_cp, # function returning a pytorch network to encode state values
-    "train_blocks": 1, # how often train is called
-    "block_size": 3000, # episodes per call to train
-    "num_workers": 4, # number of worker processes
-    "lookahead": 30, # steps to take before computing losses
-    "show_immediate": False, # show plots after each call to train
-    "keep_plots": True, # keep plots open after training has finished
-    "debug": False, # additional debug prints
-    "epsilon": annealing, # exploration strategy
-    "policy_lr": 0.0002, # learning rate for policy net optimizer
-    "value_lr": 0.0002, # learning rate for valuenet optimizer
-    "policy_decay": 0.0001, # weight decay for policy optimizer
-    "value_decay": 0.0001, # weight decay for value optimizer
-    "env": CartpoleFactory(), # environment factory object
-    "evaluate": 10, # number of episodes to play for evaluation
-    "grad_clip": 40, # max norm for gradients, used to clip gradients
-    "gamma": 0.99, # discount for future rewards
-    "actions": [0,1] # actions allowed in the environment
-}
 
 # initializes agent and runs training loop
 def main(config):
-    logger = Logger(SAVE_DIR)
-    agent = A3CAgent(config, logger)
+    queue = Queue()
+    logger = Logger(SAVE_DIR, queue)
+    agent = A3CAgent(config, queue)
 
-    plt.ion() # show plots in a non blocking way
-    for i in range(config["train_blocks"]): # train in blocks and save checkpoints
-        print(f"Starting Training Block {i}")
-        result_dict = agent.train(config["block_size"], config["num_workers"], show_plots=True, render=False)
-        agent.evaluate(config["evaluate"])
-        path = os.path.join(SAVE_DIR, f"checkpoint-{i}")
-        agent.save_model(path)
-        path = os.path.join(SAVE_DIR, f"results_oneblock_newloss")
-        with open(path, "wb") as f:
-            pickle.dump(result_dict, f)
-        with open(os.path.join(SAVE_DIR, "weight_log"), "wb") as f:
-            pickle.dump(agent.weight_log, f)
-        if config["show_immediate"]:
-            plt.draw()
-            plt.pause(1) # give pyplot time to draw the plots
-
-        if config["debug"]: # your debug statements here
-            print("########### DEBUG ############")
-            print("########### \\DEBUG ############\n\n")
-
+    log_thread = Thread(target=logger.run, name="logger")
+    try:
+        log_thread.start()
+        plt.ion() # show plots in a non blocking way
+        for i in range(config["train_blocks"]): # train in blocks and save checkpoints
+            print(f"Starting Training Block {i}")
+            result_dict = agent.train(config["block_size"], config["num_workers"], show_plots=False, render=False)
+            agent.evaluate(config["evaluate"], render=False, show_plots=False)
+            path = os.path.join(SAVE_DIR, f"checkpoint-{i}")
+            agent.save_model(path)
+            # path = os.path.join(SAVE_DIR, f"results_oneblock_newloss")
+            # with open(path, "wb") as f:
+            #     pickle.dump(result_dict, f)
+            # with open(os.path.join(SAVE_DIR, "weight_log"), "wb") as f:
+            #     pickle.dump(agent.weight_log, f)
+            if config["show_immediate"]:
+                plt.draw()
+                plt.pause(1) # give pyplot time to draw the plots
+        queue.put(None)
+        log_thread.join()
+    except KeyboardInterrupt as e:
+        queue.put(None)
+        log_thread.join()
+        raise e
 
     # write logs to disk
     logger.write()
@@ -103,4 +55,4 @@ def main(config):
 
 # if this file is called as the main entry point for the program, call the main function with parameters specified in config
 if __name__ == "__main__":
-    main(cartpole_conf)
+    main(get_config("cartpole"))
--- src/neurips2019/util/Logger.py
+++ src/neurips2019/util/Logger.py
@@ -1,39 +1,73 @@
+from collections import namedtuple
 from torch.utils.tensorboard import SummaryWriter
 import os
 import json
 import time
+import numpy as np
+from enum import Enum
+
+
+class LogType(Enum):
+    DISK=0
+    SCALAR=1
+    HISTOGRAM=2
+    IMAGE=3
+
+LogEntry = namedtuple('LogEntry', ['type', 'key', 'value', 'idx', 'kwargs'])
 
 class Logger:
     '''
     Class that manages logging statistics and makes them accessible to Tensorboard,
     utility for weight logging, saving agents
     '''
-    def __init__(self, directory):
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
+    def __init__(self, directory, queue):
+        path = os.path.abspath(directory) # in case working directory changes elsewhere keep logging location
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        
         self.disk_log = dict()
         self.tb_log = dict()
-        self.directory = directory
+        self.tb_writer = SummaryWriter(path)
+        self.directory = path
+        self.q = queue
+
+    def run(self):
+        type_switch = {
+            LogType.DISK : lambda a,b,c,d: None,
+            LogType.SCALAR : self.tb_writer.add_scalar,
+            LogType.HISTOGRAM : self.tb_writer.add_histogram,
+            LogType.IMAGE : self.tb_writer.add_image
+        }
+
+        while True:
+            entry = self.q.get()
+            if entry is None:
+                self.tb_writer.close()
+                break
+            func = type_switch[entry.type]
+            func(entry.key, entry.value, global_step=entry.idx, **entry.kwargs)
 
     def log_tb(self, key, value, idx=None, walltime=None):
         # log to dict to be written to tensorboard
         if not key in self.tb_log.keys():
             self.tb_log[key] = list()
-        self.tb_log[key] = (value, idx, walltime)
+        self.tb_log[key].append((value, idx, walltime))
 
     def log_disk(self, key, value, idx=None):
         # log to dict that will be written to disk
         if not key in self.disk_log.keys():
-            self.weight_logger[key] = list()
+            self.disk_log[key] = list()
         self.disk_log[key].append((value,idx))
 
     def write(self):
         # write logs to tensorboard
-        writer = SummaryWriter(log_dir=self.directory)
-        for key in self.tb_log.keys():
-            for value, idx, time in self.tb_log.values():
-                writer.add_scalar(tag=key, scalar_value=value, global_step=idx, walltime=time)
+        # with SummaryWriter(log_dir=self.directory) as writer:
+        #     for key, hist in self.tb_log.items():
+        #         for (value, idx, walltime) in hist:
+        #             writer.add_scalar(tag=key, scalar_value=value, global_step=idx, walltime=walltime)
         # write logs to disk
         path = os.path.join(self.directory, f"logs_{time.time()}")
         with open(path, 'w') as file:
             file.write(json.dumps(self.disk_log))
+
+
