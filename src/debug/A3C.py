8e06451f031abbf6a49fe5a625809cd070756b10
Track4 <s1044709@student.ru.nl>
2020-02-26 17:34:45 +0100

    fix loss function

--- src/debug/A3C.py
+++ src/debug/A3C.py
@@ -24,8 +24,8 @@ class A3C:
         self.policynet.share_memory()
 
         params = [self.convnet.parameters(), self.valuenet.parameters(), self.policynet.parameters()]
-        # self.optimizer = Adam(itertools.chain(*params))
-        self.optimizer = SGD(itertools.chain(*params), lr=0.0001, momentum=0.9)
+        self.optimizer = Adam(itertools.chain(*params))
+        # self.optimizer = SGD(itertools.chain(*params), lr=0.0001, momentum=0.9)
 
         self.global_counter = Value('i', 0)
 
